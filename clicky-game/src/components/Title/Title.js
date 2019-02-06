71e87055e7594597993027b5bf912c2849fd807d
aMedina9697 <aMedina9697@gmail.com>
2019-02-05 19:49:49 -0600

    final commit

---
+++ clicky-game/src/components/Title/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Title.css";
+
+const Title = props => (
+    <div className="titlebox">
+        <h1>Dog-Meme Clicky Game!</h1>
+        <p>Click a meme for earned points, but don't click the same meme twice!</p>
+    </div>
+);
+
+export default Title;
