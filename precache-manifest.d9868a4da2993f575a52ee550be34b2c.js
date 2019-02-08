b2d98b8a392f0b089285bb44d2b22059e668fb40
aMedina9697 <aMedina9697@gmail.com>
2019-02-07 18:36:45 -0600

    Updates

---
+++ precache-manifest.d9868a4da2993f575a52ee550be34b2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "c36e37d75fca30cf035d",
+    "url": "/clicky-game/static/js/main.c36e37d7.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "c36e37d75fca30cf035d",
+    "url": "/clicky-game/static/css/main.bb396ebb.chunk.css"
+  },
+  {
+    "revision": "071d2ea1e097105c643832c40e783539",
+    "url": "/clicky-game/index.html"
+  }
+];
